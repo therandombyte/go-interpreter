7401bfcd390139ce560bdaf3ade5e416dc9b9ab0
therandombyte <sasidharan.nikhil@gmail.com>
2025-08-05 00:02:42 +0800

    add: parser wiring

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"github.com/therandombyte/go-interpreter/ast"
+	"github.com/therandombyte/go-interpreter/lexer"
+	"github.com/therandombyte/go-interpreter/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
